c0f2ee3bcc00c2918b8539bb7953eb803f28283d
PǝRV <alexander.w.matulionis@gmail.com>
2018-01-03 22:52:05 -0800

    Changed Show to Anim (more generic; flexible)

--- page.go
+++ page.go
@@ -15,20 +15,39 @@ func catch(err *error) {
 func ID(id string) (e Elem, err error) {
 	defer catch(&err)
 	e = Elem{js.Global.Get("document").Call("getElementById", id)}
-	if e.Object == nil {
+	if e.js == nil {
 		err = errors.New("bad id")
 	}
 	return
 }
 
 type Elem struct {
-	*js.Object
+	js *js.Object
 }
 
-func (e *Elem) Show(b bool) {
-	a := 0
-	if b {
-		a = 1
-	}
-	e.Get("style").Set("opacity", a)
+// func (e *Elem) Translate(left, top float64) <-chan bool {
+// 	c := make(chan bool, 1)
+// 	e.js.Call("addEventListener", "transitionend", func() { c <- true }, false)
+// 	e.js.Get("style").Set("translate", left, top)
+// 	return c
+// }
+// func (e *Elem) Scale(n float64) <-chan bool {
+// 	c := make(chan bool, 1)
+// 	e.js.Call("addEventListener", "transitionend", func() { c <- true }, false)
+// 	e.js.Get("style").Set("scale", n)
+// 	return c
+// }
+// func (e *Elem) Rotate(r float64) <-chan bool {
+// 	c := make(chan bool, 1)
+// 	e.js.Call("addEventListener", "transitionend", func() { c <- true }, false)
+// 	e.js.Get("style").Set("rotate", r)
+// 	return c
+// }
+func (e *Elem) Anim(class string) (done <-chan bool, err error) {
+	defer catch(&err)
+	c := make(chan bool, 1)
+	e.js.Call("addEventListener", "animationend", func() { c <- true }, false)
+	e.js.Set("className", class)
+	done = c
+	return
 }
